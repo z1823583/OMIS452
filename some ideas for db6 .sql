e8762187273bf1126d78d9088b23cd6328f2fadb
Vikas Rao <55199529+z1823583@users.noreply.github.com>
2019-11-19 16:14:28 -0600

    Update some ideas for db6 .sql

--- some ideas for db6 .sql
+++ some ideas for db6 .sql
@@ -1,5 +1,6 @@
-//compound query 1//
+//**compound query 1 - Shows PartID with Make and Model from Vehicles Table.**//
 Select parts.PartID, Vehicles.Make, Vehicles.Model
 from parts
+where PartID > 1
 RIGHT JOIN Vehicles ON parts.Model = vehicles.Model  
 ORDER BY `parts`.`PartID`  DESC
