1e50dd53cc2779d79b9c01e311596afbfbe716e1
Vikas Rao <55199529+z1823583@users.noreply.github.com>
2019-11-20 17:30:12 -0600

    Update some ideas for db6 .sql

--- some ideas for db6 .sql
+++ some ideas for db6 .sql
@@ -13,3 +13,13 @@ where EXISTS
      from Vehicles
      where parts.Make = Vehicles.Make and Parts.Model = Vehicles.Model)
 Order By PartID
+
+
+-- Shows Vehicles info for Parts that are available in dealership
+select Vehicles.VIN, Vehicles.Make, Vehicles.Model, Vehicles.StateOfCar
+from vehicles
+WHERE vehicles.Make IN (
+    	select DISTINCT parts.Make
+	from parts
+	where parts.make = 'Ford' or parts.make = 'Nissan' or parts.Make = 'Audi'
+    );
